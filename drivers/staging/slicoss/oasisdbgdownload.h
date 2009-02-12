2f26778c5f950b6e58af693d5ba2408695bcf859
Jason Self <j@jxself.org>
2009-02-12 09:51:28 -0800

    Linux-libre 2.6.28.5-gnu1

---
+++ drivers/staging/slicoss/oasisdbgdownload.h
@@ -0,0 +1,16 @@
+#define OASIS_UCODE_VERS_STRING	"1.2"
+#define OASIS_UCODE_VERS_DATE  	"2006/03/27 15:11:22"
+#define OASIS_UCODE_HOSTIF_ID  	3
+
+static s32 ONumSections = 0x2;
+static u32 OSectionSize[] =
+{
+	0x00004000, 0x00010000,
+};
+
+static u32 OSectionStart[] =
+{
+	0x00000000, 0x00008000,
+};
+
+/*(DEBLOBBED)*/
