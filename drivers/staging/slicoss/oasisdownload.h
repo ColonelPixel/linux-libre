cb4a25c19d1437bb9afad0ec766855eed86cd57f
Jason Self <j@jxself.org>
2009-02-17 09:29:45 -0800

    Linux-libre 2.6.28.6-gnu1

---
+++ drivers/staging/slicoss/oasisdownload.h
@@ -0,0 +1,14 @@
+#define OASIS_UCODE_VERS_STRING	"1.2"
+#define OASIS_UCODE_VERS_DATE  	"2006/03/27 15:10:37"
+#define OASIS_UCODE_HOSTIF_ID  	3
+
+static s32 ONumSections = 0x2;
+static u32 OSectionSize[] = {
+	0x00004000, 0x00010000,
+};
+
+static u32 OSectionStart[] = {
+	0x00000000, 0x00008000,
+};
+
+/*(DEBLOBBED)*/
