897faca8a630f0a9a921805cfec947f426f461e2
Jason Self <j@jxself.org>
2009-07-24 14:35:00 -0700

    Linux-libre 2.6.27.28-gnu1

---
+++ drivers/net/pcmcia/ositech.h
@@ -0,0 +1,9 @@
+/*
+    This file contains the firmware of Seven of Diamonds from OSITECH.
+    (Special thanks to Kevin MacPherson of OSITECH)
+
+    This software may be used and distributed according to the terms of
+    the GNU General Public License, incorporated herein by reference.
+*/
+
+    /*(DEBLOBBED)*/
