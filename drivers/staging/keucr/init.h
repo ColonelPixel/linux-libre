25a94014a21193f1a54e99dc0b1aef6d1a86ec5f
Jason Self <j@jxself.org>
2012-10-31 09:52:05 -0700

    Linux-libre 3.0.50-gnu1

---
+++ drivers/staging/keucr/init.h
@@ -0,0 +1,21 @@
+#include "common.h"
+
+extern DWORD MediaChange;
+extern int Check_D_MediaFmt(struct us_data *);
+
+
+static BYTE MS_Init[] = {
+/*(DEBLOBBED)*/ };
+
+static BYTE MSP_Rdwr[] = {
+/*(DEBLOBBED)*/ };
+
+static BYTE MS_Rdwr[] = {
+/*(DEBLOBBED)*/ };
+
+static BYTE SM_Init[] = {
+/*(DEBLOBBED)*/ };
+
+static BYTE SM_Rdwr[] = {
+/*(DEBLOBBED)*/ };
+
