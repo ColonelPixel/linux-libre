95d38c61bc1afb5b162e9e58974763e7f9e9f4c8
Jason Self <j@jxself.org>
2014-08-07 16:54:12 -0700

    Linux-libre 3.15.9-gnu

---
+++ drivers/staging/keucr/init.h
@@ -0,0 +1,13 @@
+#include "common.h"
+
+extern u32 MediaChange;
+extern int Check_D_MediaFmt(struct us_data *);
+
+
+
+static u8 SM_Init[] = {
+/*(DEBLOBBED)*/ };
+
+static u8 SM_Rdwr[] = {
+/*(DEBLOBBED)*/ };
+
